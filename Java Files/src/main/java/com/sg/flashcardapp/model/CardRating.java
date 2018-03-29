873791a3626a251f3475782dc3ffe75b74343f50
amaxama <maxam.anna@gmail.com>
2018-03-29 16:03:46 -0500

    More additions to security

--- Java Files/src/main/java/com/sg/flashcardapp/model/CardRating.java
+++ Java Files/src/main/java/com/sg/flashcardapp/model/CardRating.java
@@ -5,6 +5,7 @@
  */
 package com.sg.flashcardapp.model;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -27,6 +28,66 @@ public class CardRating {
     
     @Column(nullable = false)
     private Integer rating;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(int cardId) {
+        this.cardId = cardId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + this.userId;
+        hash = 89 * hash + this.cardId;
+        hash = 89 * hash + Objects.hashCode(this.rating);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CardRating other = (CardRating) obj;
+        if (this.userId != other.userId) {
+            return false;
+        }
+        if (this.cardId != other.cardId) {
+            return false;
+        }
+        if (!Objects.equals(this.rating, other.rating)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     
     
 }
